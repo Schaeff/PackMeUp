e816c40f8ac42223bda0607e85c4e1d75a3d3e59
Thibaut SCHAEFFER <schaeffer.thibaut@gmail.com>
2014-12-10 00:45:32 +0100

    find_suitcase working, doc updated

--- app/controllers/suitcases_controller.rb
+++ app/controllers/suitcases_controller.rb
@@ -1,12 +1,14 @@
 class SuitcasesController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
-	def request
-		#country = Country.find_by name: suitcase_params[:country]
-		#weather = suitcase_params[:weather]
-		#@suitcase = Suitcase.find_by weather: weather
+	def find_suitcase
+		country = Country.find_by name: suitcase_params[:country]
+		weather = suitcase_params[:weather]
+		@suitcase = Suitcase.find_by weather: weather, country: country
 
-		respond_to  {render :show, location: @suitcase}
+		respond_to do |format|
+			format.json  {render json: @suitcase}
+		end
 	end
 
 	def suitcase_params
